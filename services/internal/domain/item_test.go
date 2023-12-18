4740739877ccdde8805b611c5d50bd65a3b56b7b
André Freitas <freitasandre38@gmail.com>
2023-12-18 11:00:26 -0300

    feat: create test item

---
+++ services/internal/domain/item_test.go
@@ -0,0 +1,25 @@
+package domain
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestItemJSONSerialization(t *testing.T) {
+	item := &Item{
+		Id:          1,
+		Name:        "Test Item",
+		Description: "Description of test item",
+		Price:       9.99,
+	}
+
+	data, err := json.Marshal(item)
+	assert.NoError(t, err, "Error serializing item to JSON")
+
+	var newItem Item
+	err = json.Unmarshal(data, &newItem)
+	assert.NoError(t, err, "Error deserializing JSON to item")
+	assert.Equal(t, item, &newItem, "Original item and deserialized item do not match")
+}
