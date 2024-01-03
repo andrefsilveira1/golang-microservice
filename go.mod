29d5af331c9d3c79efa331b46c39a53a38da0336
André Freitas <freitasandre38@gmail.com>
2024-01-03 08:01:32 -0300

    feat: create database connect function

--- go.mod
+++ go.mod
@@ -4,7 +4,7 @@ go 1.21.4
 
 require (
 	github.com/jmoiron/sqlx v1.3.5
-	github.com/lib/pq v1.2.0
+	github.com/lib/pq v1.10.9
 	github.com/pkg/errors v0.9.1
 )
 
---
+++ services/internal/database/postgres/database.go
@@ -0,0 +1,22 @@
+package postgres
+
+import (
+	"fmt"
+
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
+
+	"menu-service/internal/config"
+)
+
+func Connect(cfg *config.Database) (*sqlx.DB, error) {
+	dsn := fmt.Sprintf("host=%s port=%d dbname=%s user=%s password=%s sslmode=disable",
+		cfg.Host, cfg.Port, cfg.Database, cfg.Username, cfg.Password)
+
+	db, err := sqlx.Open(cfg.Driver, dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
