22200edc639be1f4329dc7f23fc1a68ee991a200
André Freitas <freitasandre38@gmail.com>
2024-01-08 09:34:24 -0300

    fix: adjust category service as parameters of function

--- services/internal/transport/rest/endpoints/category_list.go
+++ services/internal/transport/rest/endpoints/category_list.go
@@ -3,10 +3,10 @@ package endpoints
 import (
 	"net/http"
 
-	"menu-service/internal/domain"
+	"microservices/services/internal/domain"
 )
 
-func MakeListCategoryEndpoint(ItemService *domain.ItemService) http.HandlerFunc {
+func MakeListCategoryEndpoint(itemService *domain.CategoryService) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
 		http.Error(w, "Endpoint not implemented...", http.StatusInternalServerError)
