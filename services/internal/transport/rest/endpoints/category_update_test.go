7b4f107058b1da2cbe6b0211de13a43f52e33919
André Freitas <freitasandre38@gmail.com>
2024-01-08 10:28:06 -0300

    feat: create category update test

---
+++ services/internal/transport/rest/endpoints/category_update_test.go
@@ -0,0 +1,31 @@
+package endpoints
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"microservices/services/internal/domain"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMakeUpdateCategoryEndpoint(t *testing.T) {
+	categoryService := &domain.CategoryService{}
+
+	server := httptest.NewServer(MakeUpdateCategoryEndpoint(categoryService))
+	defer server.Close()
+
+	req, err := http.NewRequest("PUT", server.URL, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	assert.Equal(t, http.StatusInternalServerError, resp.StatusCode)
+}
