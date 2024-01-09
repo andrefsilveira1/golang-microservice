ce46b4a86d39a385f6a4710231326c16c4ec057b
André Freitas <freitasandre38@gmail.com>
2024-01-09 20:00:17 -0300

    feat: add new category handler

--- services/internal/transport/rest/category_handler.go
+++ services/internal/transport/rest/category_handler.go
@@ -12,3 +12,21 @@ func NewCategoryHandler(categoryService *domain.CategoryService) *CategoryHandle
 		categoryService: categoryService
 	}
 }
+
+func (h *CategoryHandler) Register (router *mux.Router) {
+	listCategoryEndpoint := endpoints.MakeListCategoryEndpoint(h.categoryService)
+	findCategoryEndpoint := endpoints.MakeFindCategoryEndpoint(h.categoryService)
+
+	router.HandleFunc("/categories", listCategoryEndpoint).Methods(http.MethodGet)
+	router.HandleFunc("/categories/{id}", findCategoryEndpoint).Methods(http.MethodGet)
+
+	protected := router.PathPrefix("/").Subrouter()
+
+	createCategoryEndpoint := endpoints.MakeCreateCategoryEndpoint(h.categoryService)
+	updateCategoryEndpoint := endpoints.MakeUpdateCategoryEndpoint(h.categoryService)
+	deleteCategoryEndpoint := endpoints.MakeDeleteCategoryEndpoint(h.categoryService)
+
+	protected.HandleFunc("/categories", createCategoryEndpoint).Methods(http.MethodPost)
+	protected.HandleFunc("/categories/{id}", updateCategoryEndpoint).Methods(http.MethodPut)
+	protected.HandleFunc("/categories/{id}", deleteCategoryEndpoint).Methods(http.MethodDelete)
+}
