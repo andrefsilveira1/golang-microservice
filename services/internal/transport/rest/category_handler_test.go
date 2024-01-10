cc309f7f973918235c804963bdf6b78eba83ec3a
André Freitas <freitasandre38@gmail.com>
2024-01-10 10:18:16 -0300

    feat: create category handler test

---
+++ services/internal/transport/rest/category_handler_test.go
@@ -0,0 +1,67 @@
+// internal/transport/rest/category_handler_test.go
+
+package rest
+
+import (
+	"net/http"
+	"testing"
+
+	"menu-service/internal/domain"
+
+	"github.com/gorilla/mux"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCategoryHandler_Routes(t *testing.T) {
+	mockCategoryService := &domain.CategoryService{}
+
+	router := mux.NewRouter()
+	categoryHandler := NewCategoryHandler(mockCategoryService)
+	categoryHandler.Register(router)
+
+	expectedRoutes := []struct {
+		method string
+		path   string
+	}{
+		{http.MethodGet, "/categories"},
+		{http.MethodGet, "/categories/{id}"},
+		{http.MethodPost, "/categories"},
+		{http.MethodPut, "/categories/{id}"},
+		{http.MethodDelete, "/categories/{id}"},
+	}
+
+	registeredRoutes := make(map[string]bool)
+
+	callback := func(route *mux.Route, router *mux.Router, ancestors []*mux.Route) error {
+		if methods, _ := route.GetMethods(); methods != nil {
+			pathTemplate, err := route.GetPathTemplate()
+			if err != nil {
+				return err
+			}
+
+			methods, err := route.GetMethods()
+			if err != nil {
+				return err
+			}
+
+			for _, method := range methods {
+				registeredRoutes[method+" "+pathTemplate] = true
+			}
+		}
+
+		return nil
+	}
+
+	err := router.Walk(callback)
+	assert.Nil(t, err, "Unexpected error while walking through the router")
+
+	for _, r := range expectedRoutes {
+		route := r
+		t.Run(route.path, func(t *testing.T) {
+			t.Parallel()
+			expectedRouteKey := route.method + " " + route.path
+
+			assert.True(t, registeredRoutes[expectedRouteKey], "Expected route %v is not registered", expectedRouteKey)
+		})
+	}
+}
