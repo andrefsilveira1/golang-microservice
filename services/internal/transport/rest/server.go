1f06520a13af5a5f50ed5263ed358c0aed660b4e
André Freitas <freitasandre38@gmail.com>
2024-01-17 09:27:39 -0300

    feat: create start server function

--- services/internal/transport/rest/server.go
+++ services/internal/transport/rest/server.go
@@ -26,3 +26,26 @@ func NewServer(config *config.ServerHTTP, router *mux.Router) *Server {
 		Config: config
 	}
 }
+
+func (s *Server) Start() error {
+	var err error
+	log.Printf("HTTP server starting at: '%s: %d \n' ", s.Config.Host, s.Config.Port)
+	if s.Config.UseHTTPS {
+		log.Println("SSL certificate Enabled")
+		path := s.Config.CerPath
+		err = s.Server.ListenAndServeTLS(
+			fmt.Sprintf("%s /server.crt", path),
+			fmt.Sprintf("%s /server.key", path),
+		)
+	} else {
+		log.Println("SSL certificate Disabled")
+		err = s.Server.ListenAndServe()
+	}
+
+	if err != nil && err != http.ErrServerClosed {
+		log.Printf("Unable to start HTTP server: %+v\n", err)
+		return err
+	}
+
+	return nil
+}
